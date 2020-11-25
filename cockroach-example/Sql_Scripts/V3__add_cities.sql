01a20273a6812425ef739a86e2a8344e4e508789
Andreas Lunderhage <lunderhage@gmail.com>
2020-11-25 09:43:47 +0100

    SQL Server & Cockroach Migration V3

---
+++ cockroach-example/Sql_Scripts/V3__add_cities.sql
@@ -0,0 +1,7 @@
+use people_registry;
+
+update people set city = 'Stockholm' where firstname = 'Ture' and surname = 'Sventon';
+update people set city = 'Bofors' where firstname = 'Allan' and surname = 'Svensson';
+update people set city = 'Halkstad' where firstname = 'Hubert' and surname = 'Trehuvud';
+update people set city = 'Kiruna' where firstname = 'Snyggve' and surname = 'Tryggvesson';
+update people set city = 'Bollstad' where firstname = 'Boll-Kalle' and surname = 'Pallesson';
